5307dfd37163e75cae98000c9623b987ac864b38
Mr-A12k <kabil448446@gmail.com>
2024-07-18 07:24:46 -0700

    push

--- ArithmeticOperations.java
+++ ArithmeticOperations.java
@@ -39,7 +39,7 @@ public class ArithmeticOperations {
         return a * b;
     }
 
-    // Method to perform division
+
     public static double divide(double a, double b) {
         if (b == 0) {
             System.out.println("Error! Division by zero is not allowed.");
